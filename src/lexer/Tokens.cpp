28053a0264937e4b488a5418b7e7c5cd3b16413e
Trimsku <69107937+Trimsku@users.noreply.github.com>
2020-11-30 16:36:31 +0300

    Update Tokens.cpp

--- src/lexer/Tokens.cpp
+++ src/lexer/Tokens.cpp
@@ -2,29 +2,45 @@
 #include "../ParsSyn.h"
 namespace Ow::Lexer
 {
-    class gettok{
+    class ToPeek{
         public:
-        gettok(const char* str){
-            str_peeks = str;
+        ToPeek(const char* str){
+            str_peeks = GetInStr(str);
         }
         std::string str_peeks;
         int index = 0;
         int line = 1;
-        int column = 1;
         std::string str;
-            char peek();
-            char next_peek(int advance);
-            char to_next_peek(int advance){
-                return advance;
+            char peek(){
+                return str_peeks[index];
             }
-
+            char next_peek(int advance = 1){
+                return str_peeks[index+advance];
+            }
+            void to_next_peek(int advance = 1){
+                while(advance){
+                    if(str_peeks[index] == '\n') line++;
+                    
+                    index++;
+                    advance--;
+                };
+            }
+            
+        
+    };
+    class ParseSyntax {
+        //Logic Functions
+        bool GoSkip(const char _True){
+            return _True == ' ' || _True == '\t' || _True == '\r';
+        }
+        bool is_digit(const char _True){
+            return _True >= '0' && _True <= '9';        }
+        bool _String(const char _True){
+            return isalpha(_True) || _True == '_';
+        }
+        bool is_startorend_str(const char _True){
+            return _True == '"' || _True == '\'' || _True == '`';
+        }
     };
     
-    
 } // namespace Ow::Lexer
-
-int main(){
-    Ow::Lexer::gettok a("test/print.ow");
-    a.to_next_peek(3);
-    return 0;
-}
